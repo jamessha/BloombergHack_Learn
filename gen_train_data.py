converted to moving average, bug fixes, 0.69 test accuracy

gen_train_data.py
@@ -14,6 +14,12 @@ def parseDatetimeString(s):
   return date
 
 
+def moving_average(a, n=3) :
+  ret = np.cumsum(a, dtype=float)
+  ret[n:] = ret[n:] - ret[:-n]
+  return ret[n - 1:] / n
+
+
 def main():
   f = open('data/ticker_data.pickle')
   data = pickle.load(f)
@@ -26,23 +32,36 @@ def main():
   test_y = []
   for idx, (ticker, datum) in enumerate(data['tickers'].items()):
     print '{0}/{1}'.format(idx+1, len(data['tickers'].keys()))
-    r = random.random()
-    X = train_X
-    y = train_y
-    if r < 0.1:
-      X = test_X
-      y = test_y
-    for i in xrange(1, len(datum)):
-      _, prev_val = datum[i-1]
+    n = 3
+    vals = []
+    dates = []
+    for i in xrange(len(datum)):
       date, val = datum[i]
-      delta = (val - prev_val)/prev_val
+      vals.append(val)
+      dates.append(date)
+    vals = np.array(vals)
+    vals = moving_average(vals, n=n)
+    dates = dates[:-(n-1)]
+
+    for i in xrange(1, len(dates)-1):
+      r = random.random()
+      X = train_X
+      y = train_y
+      if r < 0.1:
+        X = test_X
+        y = test_y
+      prev_val = vals[i-1]
+      next_val = vals[i+1]
+      val = vals[i]
+      date = dates[i]
+      delta = (next_val - val)/val
       thresh = 0.01
-      if delta < thresh:
-        label = 1
+      if delta < -thresh:
+        label = 0.0
       elif delta > thresh:
-        label = 2
+        label = 1.0
       else:
-        label = 0
+        label = (delta + thresh)/2/thresh
 
 
       year, month, day = date
@@ -60,8 +79,10 @@ def main():
           avg_sent += sent
         avg_sent /= (len(items) - 4)
 
-      X.append([avg_sent, prev_val, val])
+      X.append([avg_sent, val, prev_val])
       y.append(label)
+
+      print (next_val-val)/val, avg_sent
 
   train_X = np.array(train_X)
   train_y = np.array(train_y)
@@ -73,13 +94,6 @@ def main():
   np.save('data/train_y.npy', train_y)
   np.save('data/test_X.npy', test_X)
   np.save('data/test_y.npy', test_y)
-
-  # Calvin examples
-  #rss_url = 'http://finance.yahoo.com/rss/headline?s=%s' % ticker
-  #feed = feedparser.parse(rss_url)
-  #for item in feed['items']:
-  #  item['title']
-
 
 if __name__ == '__main__':
   main()

train.py
@@ -4,28 +4,50 @@ from sklearn import svm, preprocessing
 
 def test(clf, X, y):
   z = clf.predict(X)
-  diff = z - y
-  errors = sum(abs(diff))
-  acc = 1 - errors/X.shape[0]
+  thresh = 1
+  for i in xrange(z.shape[0]):
+    if z[i] < -thresh:
+      z[i] = 0
+    elif z[i] > thresh:
+      z[i] = 1
+    else:
+      z[i] = round(z[i])
+  diff = np.equal(z, np.round(y))
+  correct = float(abs(diff).sum())
+  acc = correct/X.shape[0]
   return acc
 
 
 def train(X, y):
-  clf = svm.SVC()
+  clf = svm.SVR(kernel='rbf', C=1e3, gamma=0.1)
+  #print X[2:4, :]
+  #print y[2:4]
   clf.fit(X, y)
   return clf
 
 
 def main():
-  X = np.load('data/train_X.npy')
-  y = np.load('data/train_y.npy')
+  train_X = np.load('data/train_X.npy')
+  train_y = np.load('data/train_y.npy')
+  test_X = np.load('data/test_X.npy')
+  test_y = np.load('data/test_y.npy')
+  print 'Training examples:', train_X.shape[0]
+  print 'Testing examples:', test_X.shape[0]
 
-  preprocessing.scale(X, copy=False)
+  preprocessing.scale(train_X, copy=False)
+  preprocessing.scale(test_X, copy=False)
   #preprocessing.normalize(X, norm='l2', axis=1, copy=False)
 
-  clf = train(X, y)
-  acc = test(clf, X, y)
-  print 'training accuracy', acc
+  #print train_y
+  clf = train(train_X, train_y)
+  dec = clf.decision_function(train_X[3, :])
+  print dec
+  train_acc = test(clf, train_X, train_y)
+  test_acc = test(clf, test_X, test_y)
+  print 'training accuracy', train_acc
+  print 'testing accuracy', test_acc
+
+  print clf.predict([[0.8, 0.1, 0.5]])
 
 
 if __name__ == '__main__':